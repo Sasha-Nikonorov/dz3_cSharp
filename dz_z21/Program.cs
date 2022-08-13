e308145e3945103e02c769b73ef6214a2d23d135
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-13 17:56:06 +0300

    3d x y z coordinate

---
+++ dz_z21/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите x1:");
+var x1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите y1:");
+var y1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите z1:");
+var z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите x2:");
+var x2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите y2:");
+var y2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите z2:");
+var z2 = int.Parse(Console.ReadLine()!);
+
+double distance = Math.Sqrt((Math.Pow(x2 - x1, 2) +
+                            Math.Pow(y2 - y1, 2) +
+                            Math.Pow(z2 - z1, 2)));
+Console.WriteLine("\n" + distance.ToString("N2"));
