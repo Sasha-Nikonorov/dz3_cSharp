9cf90096631421df7f4926fc442a82bc4ff7ef62
Sasha-Nikonorov <110184933+Sasha-Nikonorov@users.noreply.github.com>
2022-08-13 18:34:28 +0300

    Update Program.cs

--- dz_z21/Program.cs
+++ dz_z21/Program.cs
@@ -16,7 +16,7 @@ var y2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите z2:");
 var z2 = int.Parse(Console.ReadLine()!);
 
-double distance = Math.Sqrt((Math.Pow(x2 - x1, 2) +
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) +
                             Math.Pow(y2 - y1, 2) +
-                            Math.Pow(z2 - z1, 2)));
+                            Math.Pow(z2 - z1, 2));
 Console.WriteLine("\n" + distance.ToString("N2"));
