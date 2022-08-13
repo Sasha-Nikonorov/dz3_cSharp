2ec6075d525fc253134db4565d2d2e71d5e03130
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-13 18:32:26 +0300

    palindrome chek program

---
+++ dz_z19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите:");
+var number = int.Parse(Console.ReadLine()!);
+
+int a = number / 10000;
+int b = (number/1000) - ((number/10000)*10);
+int c = (number/100) - ((number/1000)*10);
+int d = (number/10) - ((number/100)*10);
+int e = number%10;
+
+if (a == e && b ==d)
+{
+    Console.WriteLine("Да");
+}
+else if (number >= 100000)
+Console.WriteLine($"Введи правильное число");
+
+else
+Console.WriteLine("Нет");
